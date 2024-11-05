ff886f0a5b0aa1d5d6e9d81863407e1fa43d43f4
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2024-11-05 13:28:00 +0100

    Created parent vehicle class

---
+++ SaToNiRo/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaToNiRo
+{
+    public abstract class Vehicle
+    {
+        public string RegisteringNumber { get; set; }
+        public string Color { get; set; }
+        public int Wheels { get; set; }
+
+        
+    }
+}
