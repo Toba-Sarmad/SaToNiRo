19d69cced4b28d24ce4d0524699992dc45de1937
Niklas Färeborg <Fareborg@hotmail.com>
2024-11-06 09:42:48 +0100

    Created class for parkinglot

---
+++ SaToNiRo/Parkinglot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaToNiRo
+{
+    public class Parkinglot
+    {
+        private double totalNumOfParkingSpots = 10;
+        private List<Vehicle> parkedVehicles = new List<Vehicle>();
+        private int totalNumOfChargingStations = 2;
+
+    }
+}
