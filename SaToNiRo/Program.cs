8fe50389c63e30d9b389cafaadc789331026deca
Toba-Sarmad <sarmadtoba@gmail.com>
2024-11-11 11:34:57 +0100

    The whole team created built validation

--- SaToNiRo/Program.cs
+++ SaToNiRo/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SaToNiRo
+﻿using System.ComponentModel.Design;
+
+namespace SaToNiRo
 {
     internal class Program
     {
@@ -10,13 +12,51 @@
             while (true)
             {
                 Meny(parkinglot);
+
             }
+
         }
 
         public static void Meny(Parkinglot parkinglot)
         {
-            
+            Console.WriteLine("Välkomen till SaToNiRos parkeringshus\n\n ");
 
+            Console.WriteLine("Välj att logga in som: \n");
+            
+            Console.WriteLine("1: Kund");
+            Console.WriteLine("2: Ägare ");
+            Console.WriteLine("3: Parkeringsvakt ");
+            Console.WriteLine("4: Avsluta program ");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (userInput)
+            {
+                case 1: 
+                   
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+
+                    break;
+
+                default:
+                    Console.WriteLine("Ogiltigt val, försök igen ");
+                    break;
+            }
+        }
+
+        public static void UserMenu(Parkinglot parkinglot)
+        {
             Console.WriteLine("Välkomen till SaToNiRos parkeringshus\n\n ");
 
             Console.WriteLine("Gör följande val \n");
@@ -31,7 +71,7 @@
             switch (userInput)
             {
                 case 1:
-                CustomerView.ChooseVehicle(parkinglot);
+                    CustomerView.ChooseVehicle(parkinglot);
                     break;
 
                 case 2:
@@ -50,6 +90,50 @@
             }
         }
 
-        
+        public static void LoginAuth()
+        {
+            int input = 0;
+
+            string user = "user";
+            string userPass = "user";
+            string owner = "admin";
+            string ownerPass = "admin";
+            string guard = "guard";
+            string guardPass = "guard";
+
+            Console.WriteLine("Användarnamn: ");
+
+            string username = Console.ReadLine();
+            Console.WriteLine("Lösenord: ");
+            string password = Console.ReadLine();
+
+            if(username == user && password == userPass)
+            {
+                input = 1;
+            }
+            else if(username == owner && password == ownerPass)
+            {
+                input = 2;
+            }
+            else if(username == guard && password == guardPass)
+            {
+                input = 3;
+            }
+
+            switch(input)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+                    
+            }
+        }
+    
     }
+    
 }
